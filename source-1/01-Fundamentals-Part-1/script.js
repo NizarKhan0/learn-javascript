0a77a7034b3547b9410d405c93ca21110fb31be4
nizarkhan0 <nizarkhan7071@gmail.com>
2025-02-16 00:21:12 +0800

    data types

--- source-1/01-Fundamentals-Part-1/script.js
+++ source-1/01-Fundamentals-Part-1/script.js
@@ -1,10 +1,13 @@
+//values and variables
+
+/*
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
 
 //utk access dev console
 console.log(11 + 8 + 23 - 8);
 
-//values and variables
+
 console.log("Nizar");
 console.log(23);
 
@@ -16,14 +19,14 @@ console.log(firstName);
 //conventions for naming variables better lowercase
 //camelCase
 let firstNamePerson = "Nizar";
-//PascalCase
+// PascalCase
 let FirstNamePerson = "Nizar";
-//snake_case
+// snake_case
 let first_name = "Nizar";
 let $php = "php";
 let PI = 3.1415;
 
-// console.log(firstNamePerson);
+console.log(firstNamePerson);
 
 // LECTURE: Values and Variables
 // 1. Declare variables called 'country', 'continent' and 'population' and
@@ -37,3 +40,48 @@ let population = 32;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+*/
+
+
+//Data Types
+
+
+//bool to string
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Nizar");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//undifined to number
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2001;
+console.log(year);
+console.log(typeof year);
+
+//null
+console.log(typeof null);
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
