e0cccce8ba7e3f75a83dd40760df2873b9c29aae
nizarkhan0 <nizarkhan7071@gmail.com>
2025-02-15 23:11:22 +0800

    values and variables

--- source-1/01-Fundamentals-Part-1/script.js
+++ source-1/01-Fundamentals-Part-1/script.js
@@ -1,5 +1,39 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
 //utk access dev console
 console.log(11 + 8 + 23 - 8);
+
+//values and variables
+console.log("Nizar");
+console.log(23);
+
+let firstName = "Shari";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//conventions for naming variables better lowercase
+//camelCase
+let firstNamePerson = "Nizar";
+//PascalCase
+let FirstNamePerson = "Nizar";
+//snake_case
+let first_name = "Nizar";
+let $php = "php";
+let PI = 3.1415;
+
+// console.log(firstNamePerson);
+
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Malaysia";
+let continent = "Asia";
+let population = 32;
+
+console.log(country);
+console.log(continent);
+console.log(population);
