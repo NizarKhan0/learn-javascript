d0cc3913f04f78254ea553bda716607ba3d98f06
nizarkhan0 <nizarkhan7071@gmail.com>
2025-02-22 21:21:40 +0800

    conditional (ternary) operator

--- source-1/01-Fundamentals-Part-1/script.js
+++ source-1/01-Fundamentals-Part-1/script.js
@@ -425,6 +425,8 @@ console.log(n);
 ! = NOT
 */
 
+/*
+
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
 
@@ -492,7 +494,6 @@ if(dolphineScore > koalaScore){
 }
 
 
-
 //Switch statements
 
 
@@ -534,3 +535,39 @@ if (day === 'monday') {
 }else{
     console.log('Not a valid day!');
 }
+
+*/
+
+//Statement and expressions
+
+3 * 4
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+console.log(`I'm ${2037 - 1991} years old`);
+
+//Conditional (ternary) operator
+
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
