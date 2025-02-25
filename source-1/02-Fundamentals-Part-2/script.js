9d29c8c11f733c7e61201db67cc331574939d457
nizarkhan0 <nizarkhan7071@gmail.com>
2025-02-25 22:44:34 +0800

    functions (dry)

--- source-1/02-Fundamentals-Part-2/script.js
+++ source-1/02-Fundamentals-Part-2/script.js
@@ -1,10 +1,40 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+
+//function 
+
+function logger()
+{
+    console.log('My name is Nizar');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges)
+{
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+// console.log(fruitProcessor(2, 4));
