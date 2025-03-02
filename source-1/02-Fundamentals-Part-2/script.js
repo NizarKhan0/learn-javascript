f77db59f89b98c13882fa5b9e9868dd5f2cb19da
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-02 16:46:22 +0800

    basic array

--- source-1/02-Fundamentals-Part-2/script.js
+++ source-1/02-Fundamentals-Part-2/script.js
@@ -166,15 +166,75 @@ console.log(appleOrangeJuice);
 
 //Exercise
 
-const calcAge = function (birthYear) {
-    return 2025 - birthYear;
+// const calcAge = function (birthYear) {
+//     return 2025 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// const ages = [age1, age2, age3];
+// console.log(ages);
+
+
+//Basic Array
+
+//push method (add element to the end of the array)
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLength = friends.push('Nizar');
+console.log(friends);
+console.log(newLength);
+
+//unshift method (add element to the beginning of the array)
+friends.unshift('Zoro');
+console.log(friends);
+
+//pop method (remove element from the end of the array)
+const popped = friends.pop();
+console.log(friends);
+//utk tahu element yg di remove
+console.log(popped);
+
+//shift method (remove element from the beginning of the array)
+const shifted = friends.shift();
+console.log(friends);
+//utk tahu element yg di remove
+console.log(shifted);
+
+//indexOf method (mencari index element dalam array)
+console.log(friends.indexOf('Steven'));
+//xde
+// console.log(friends.indexOf('Zuss'));
+
+//includes method (mencari apakah ada element dalam array)
+console.log(friends.includes('Steven'));
+//false
+// console.log(friends.includes('Zuss'));
+
+friends.push(23);
+console.log(friends.includes('Nizar'));
+console.log(friends.includes('Luffy'));
+console.log(friends.includes(23));
+
+if(friends.includes('Steven')){
+    console.log('You have a friend called Steven');
+}else{
+    console.log('You do not have a friend called Steven');
 }
 
-const years = [1990, 1967, 2002, 2010, 2018];
+//Coding Challenge
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-const ages = [age1, age2, age3];
-console.log(ages);
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+
 
