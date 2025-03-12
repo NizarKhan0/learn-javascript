03bf8b63baf3e20b9606a4ce1a1bc4e9dabe2042
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-12 21:10:53 +0800

    modal function

--- source-1/06-Modal/starter/script.js
+++ source-1/06-Modal/starter/script.js
@@ -10,8 +10,43 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    // console.log(btnsOpenModal[i].textContent);
+    const btnOpenModal = btnsOpenModal[i];
+    console.log(btnOpenModal);
+    btnOpenModal.addEventListener('click', function() {
+        console.log('Button clicked');
+        // modal.style.display = 'block';
+        // overlay.style.display = 'block';
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+    const openModal = function() {
+        console.log('Button clicked');
+        // modal.style.display = 'block';
+        // overlay.style.display = 'block';
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    const closeModal = function() {
+        //utk ddapat tau button close di klik
+        console.log('Button close clicked');
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+    // overlay.addEventListener('click', function() {
+    //     //utk ddapat tau overlay di klik
+    //     console.log('overlay clicked');
+    //     modal.classList.add('hidden');
+    //     overlay.classList.add('hidden');
+    // });
 }
 
 // let test = 'test';
 // console.log(test);
+
