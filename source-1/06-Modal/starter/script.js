c2d2160c668ae8d1c56e8640bdae0718af8e2bb9
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-13 21:09:28 +0800

    esc key press

--- source-1/06-Modal/starter/script.js
+++ source-1/06-Modal/starter/script.js
@@ -45,6 +45,15 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     //     modal.classList.add('hidden');
     //     overlay.classList.add('hidden');
     // });
+
+    document.addEventListener('keydown', function(e) {
+        console.log('A key was pressed');
+        //utk check key yg di tekan
+        console.log(e.key);
+        if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    });
 }
 
 // let test = 'test';
