367a8de99421885e7b63a58e4d6094d880247e1f
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-21 21:09:20 +0800

    maps iteration

--- source-1/09-Data-Structures-Operators/starter/script.js
+++ source-1/09-Data-Structures-Operators/starter/script.js
@@ -48,8 +48,45 @@ const restaurant = {
   },
 };
 
+//Maps Iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+//Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+console.log([...question.entries()]);
+
 //Maps Fundamentals
 
+/*
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
@@ -77,6 +114,8 @@ rest.set(arr
   , 'Test');
 rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
+*/
+
 
 //Sets
 
