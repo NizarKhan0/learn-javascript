1409b9129afcf376c882efefb0e5f4882fb4548f
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-22 18:25:10 +0800

    working with strings pt 2

--- source-1/09-Data-Structures-Operators/starter/script.js
+++ source-1/09-Data-Structures-Operators/starter/script.js
@@ -89,6 +89,64 @@ console.log(new String('nizar'));
 console.log(typeof new String('nizar'));
 console.log(typeof new String('nizar').slice(1));
 
+//Working with Strings - Part 2
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//Fix capitalization in name
+
+const passenger = 'NiZar'; //Nizar
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//Comparing emails
+const email = 'qM6oP@example.com';
+const loginEmail = '  qM6oP@example.com \n';
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+//replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+//Alternative solution to replaceAll with regular expression
+console.log(announcement.replace(/door/g, 'gate'));
+
+//Booleans
+const plane2 = 'Airbus A320neo';
+console.log(plane2.includes('A320'));
+console.log(plane2.startsWith('Airbus')); //true
+console.log(plane2.startsWith('Boeing')); //false
+
+if (plane2.startsWith('Airbus') && plane2.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+//Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
 
 //Maps Iteration
 
