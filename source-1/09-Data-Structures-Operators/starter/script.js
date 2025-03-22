3f96cb906be2adba3d54e62b40f9d9223559d110
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-22 18:07:01 +0800

    working with strings pt 1

--- source-1/09-Data-Structures-Operators/starter/script.js
+++ source-1/09-Data-Structures-Operators/starter/script.js
@@ -48,7 +48,51 @@ const restaurant = {
   },
 };
 
+//Working with Strings - Part 1
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat :(');
+  else console.log('You got lucky :)');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('nizar'));
+console.log(typeof new String('nizar'));
+console.log(typeof new String('nizar').slice(1));
+
+
 //Maps Iteration
+
+/*
 
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
@@ -83,6 +127,8 @@ console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 console.log([...question.entries()]);
+
+*/
 
 //Maps Fundamentals
 
