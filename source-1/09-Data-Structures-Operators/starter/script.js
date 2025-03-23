ba671d2a8a663ace93684c3e0e3aa35dfc984705
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-23 16:31:54 +0800

    working with strings pt 3

--- source-1/09-Data-Structures-Operators/starter/script.js
+++ source-1/09-Data-Structures-Operators/starter/script.js
@@ -143,9 +143,59 @@ const checkBaggage = function (items) {
   }
 };
 
-checkBaggage('I have a laptop, some Food and a pocket Knife');
-checkBaggage('Socks and camera');
-checkBaggage('Got some snacks and a gun for protection');
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+
+//Working with Strings - Part 3
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Nizar Khan'.split(' '));
+
+const [firstName, lastName] = 'Nizar Khan'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('nizar khan');
+
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Nizar'.padStart(25, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(1234567890));
+console.log(maskCreditCard('1234567890'));
+console.log(maskCreditCard(1234));
+
+const message2 = 'Bad weather... All Departures Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
 
 
 //Maps Iteration
