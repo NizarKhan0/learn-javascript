848c0572c813933cd3f08f6f5f7d9ba15ef6c319
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-24 21:11:46 +0800

    default parameters

---
+++ source-1/10-Functions/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+
+    // ES6 Default Parameters
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
