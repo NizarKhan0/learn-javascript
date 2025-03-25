5581156589dc81871b11979666385c3dcbc1cbc4
nizarkhan0 <nizarkhan7071@gmail.com>
2025-03-25 12:23:24 +0800

    value vs reference

--- source-1/10-Functions/script.js
+++ source-1/10-Functions/script.js
@@ -21,3 +21,39 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 
 createBooking('LH123');
 createBooking('LH123', 2, 800);
+
+//Passing arguments: Value vs Reference
+
+const flight = 'LH234';
+const nizar = {
+  name: 'Nizar Khan',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+checkIn(flight, nizar);
+console.log(flight);
+console.log(nizar);
+
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = nizar;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+}
+
+newPassport(nizar);
+checkIn(flight, nizar);
+
