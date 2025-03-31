aac99ee4e1d859f9772c42cb0042be3cd209f536
Nizar Khan <nizarkhan7071@gmail.com>
2025-03-31 19:48:10 +0800

    closures

--- source-1/10-Functions/script.js
+++ source-1/10-Functions/script.js
@@ -230,3 +230,22 @@ runOnce();
 
 //console.log(isPrivate)
 console.log(notPrivate); // 46
+
+
+//Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker(); // 1 passengers
+booker(); // 2 passengers
+booker(); // 3 passengers
+
+console.dir(booker); // [Function (anonymous)]
