87ef6b6e79da09b50d9f027a0b89597091186d0d
Nizar Khan <nizarkhan7071@gmail.com>
2025-04-04 20:56:07 +0800

    looping arrays foreach

--- source-1/11-Arrays-Bankist/script.js
+++ source-1/11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -136,6 +136,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const str4 = arr.join(' - ');
 // console.log(str4); // 'a - b - c - d - e'
 
+/*
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
@@ -146,3 +147,32 @@ console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
 
 console.log('Nizar'.at(2)); // N
+*/
+
+
+//Looping array forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// 0: function(200);
+// 1: function(450);
+// 2: function(-400);
