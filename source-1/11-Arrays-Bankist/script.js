6b0e4948af451f009117f8a96f1c98be8ef6a767
nizarkhan0 <nizarkhan7071@gmail.com>
2025-04-12 22:08:25 +0800

    find method

--- source-1/11-Arrays-Bankist/script.js
+++ source-1/11-Arrays-Bankist/script.js
@@ -609,3 +609,14 @@ const overalBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
 */
+
+//The find Method
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
