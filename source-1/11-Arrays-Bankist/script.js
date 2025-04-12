2a5e783badee69cc0b628d360706903b4502605b
nizarkhan0 <nizarkhan7071@gmail.com>
2025-04-13 02:44:50 +0800

    transfer function

--- source-1/11-Arrays-Bankist/script.js
+++ source-1/11-Arrays-Bankist/script.js
@@ -129,44 +129,6 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
-
-//event handler (login)
-btnLogin.addEventListener('click', function (e) {
-  // Prevent form from submitting
-  e.preventDefault();
-
-  // console.log('LOGIN');
-
-  currentAccount = accounts.find(
-    acc => acc.username === inputLoginUsername.value
-  );
-  console.log(currentAccount);
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // console.log('LOGIN');
-    // Display UI and message
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(' ')[0]
-    }`;
-    containerApp.style.opacity = 1;
-
-    // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
-
-    // Update UI
-    updateUI(currentAccount);
-
-  } else {
-    // console.log('WRONG PIN');
-    labelWelcome.textContent = 'Wrong username or pin!';
-    containerApp.style.opacity = 0;
-    // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
-  }
-});
-
 const updateUI = function (acc) {
   // Display movements
   displayMovements(acc.movements);
@@ -243,29 +205,6 @@ btnLoan.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
-});
-
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (
-    inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
-    );
-    console.log(index);
-    // .indexOf(23)
-
-    // Delete account
-    accounts.splice(index, 1);
-
-    // Hide UI
-    containerApp.style.opacity = 0;
-  }
-
-  inputCloseUsername.value = inputClosePin.value = '';
 });
 
 let sorted = false;
@@ -507,7 +446,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-
+/*
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
@@ -545,7 +484,9 @@ const totalDepositsUSD = movements
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
 
+///////////////////////////////////////
 // Coding Challenge #3
 
 /* 
@@ -568,7 +509,7 @@ const calcAverageHumanAge = ages =>
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
-
+*/
 
 ///////////////////////////////////////
 // The find Method
@@ -581,80 +522,3 @@ console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
 
-
-///////////////////////////////////////
-// The New findLast and findLastIndex Methods
-
-console.log(movements);
-const lastWithdrawal = movements.findLast(mov => mov < 0);
-console.log(lastWithdrawal);
-
-// 'Your latest large movement was X movements ago'
-
-const latestLargeMovementIndex = movements.findLastIndex(
-  mov => Math.abs(mov) > 1000
-);
-console.log(latestLargeMovementIndex);
-console.log(
-  `Your latest large movement was ${
-    movements.length - latestLargeMovementIndex
-  } movements ago`
-);
-
-
-///////////////////////////////////////
-// some and every
-console.log(movements);
-
-// EQUALITY
-console.log(movements.includes(-130));
-
-// SOME: CONDITION
-console.log(movements.some(mov => mov === -130));
-
-const anyDeposits = movements.some(mov => mov > 0);
-console.log(anyDeposits);
-
-// EVERY
-console.log(movements.every(mov => mov > 0));
-console.log(account4.movements.every(mov => mov > 0));
-
-// Separate callback
-const deposit = mov => mov > 0;
-console.log(movements.some(deposit));
-console.log(movements.every(deposit));
-console.log(movements.filter(deposit));
-
-
-///////////////////////////////////////
-// flat and flatMap
-const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
-console.log(arr.flat());
-
-const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-console.log(arrDeep.flat(2));
-
-// flat
-const overalBalance = accounts
-  .map(acc => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance);
-
-// flatMap
-const overalBalance2 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance2);
-*/
-
-//The find Method
-
-const firstWithdrawal = movements.find(mov => mov < 0);
-console.log(movements);
-console.log(firstWithdrawal);
-
-console.log(accounts);
-
-const account = accounts.find(acc => acc.owner === 'Jessica Davis');
-console.log(account);
