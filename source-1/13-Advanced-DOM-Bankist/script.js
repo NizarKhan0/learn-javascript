96e9ec89debb72861a42a01ee53be44b38607b53
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-05 21:39:43 +0800

    smooth scrolling

--- source-1/13-Advanced-DOM-Bankist/script.js
+++ source-1/13-Advanced-DOM-Bankist/script.js
@@ -123,3 +123,33 @@ logo.classList.contains('new-class');
 
 // Don't use
 logo.className = 'new-class'; // overwrite all classes
+
+// implementing smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  //current scroll position (scrollY)
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  //height/width of viewport window position
+  console.log('height/width of viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // #scroll to the top of the page
+
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
