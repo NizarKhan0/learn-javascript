9b95d048e34324d8c0d3efb9a661d7360ee2107a
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-15 22:29:47 +0800

    sticky navigation

--- source-1/13-Advanced-DOM-Bankist/script.js
+++ source-1/13-Advanced-DOM-Bankist/script.js
@@ -175,12 +175,24 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 
-// Passing "argument" into handler
-
-
 ///////////////////////////////////////
-// Sticky navigation: Intersection Observer API
+// Sticky navigation
 
+// for get top position of the element value
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function (e) {
+  // console.log(e);
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+
+});
 
 ///////////////////////////////////////
 // Reveal sections
