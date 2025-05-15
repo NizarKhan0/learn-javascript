cbd735a98db2fd73582584a8be77bb522e0c302f
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-16 00:14:56 +0800

    lazy loading images

--- source-1/13-Advanced-DOM-Bankist/script.js
+++ source-1/13-Advanced-DOM-Bankist/script.js
@@ -246,7 +246,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -267,6 +267,34 @@ allSections.forEach(function (section) {
 
 // Lazy loading images
 
+const imgTargets = document.querySelectorAll('img[data-src]');
+// console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => {
+  imgObserver.observe(img);
+});
 
 
 ///////////////////////////////////////
