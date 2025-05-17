01f41553ea97a29620183d02ddfe4bf7f5f58f93
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-17 20:54:11 +0800

    Lifecycle DOM Events

--- source-1/13-Advanced-DOM-Bankist/script.js
+++ source-1/13-Advanced-DOM-Bankist/script.js
@@ -528,3 +528,18 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 });
 
 */
+
+// Lifecycle DOM Events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
