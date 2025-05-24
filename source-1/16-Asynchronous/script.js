2f4da6638446d152557bfc1442860449f297ebe9
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-24 22:40:43 +0800

    callback hell

--- source-1/16-Asynchronous/script.js
+++ source-1/16-Asynchronous/script.js
@@ -11,6 +11,23 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const renderCountry = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+            <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
 // AJAX call using XMLHttpRequest
 const getCountyData = function (country) {
     // Create a new XMLHttpRequest object
@@ -28,26 +45,27 @@ const getCountyData = function (country) {
         const [data] = JSON.parse(this.responseText);
         console.log(data);
 
-        // Create HTML structure for the country data
-        const html = `
-        <article class="country">
-            <img class="country__img" src="${data.flags.svg}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name.common}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
-                <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-                <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
-            </div>
-        </article>
-        `;
-        
-        // Insert the HTML into the countries container
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
+        // Render the country data
+        renderCountry(data);
+
+        //Get neighboring countries
+        const neighbors = data.borders;
+        if (!neighbors) return;
+        neighbors.forEach(neighbor => {
+            // Fetch data for each neighboring country
+            const neighborRequest = new XMLHttpRequest();
+            neighborRequest.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`, true);
+            neighborRequest.send();
+
+            neighborRequest.addEventListener('load', function () {
+                const [neighborData] = JSON.parse(this.responseText);
+                renderCountry(neighborData, 'neighbour');
+            });
+        });
     });
 };
 // Example usage of the getCountyData function
 getCountyData('Portugal');
 getCountyData('Malaysia');
 getCountyData('Singapore');
+
